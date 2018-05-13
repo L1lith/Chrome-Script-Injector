af3f32e621b3f251916b4c8bebd8b87247badfec
L1lith <lily@lillith.pw>
2018-05-12 20:31:18 -0600

    improve popup

--- popup.js
+++ popup.js
@@ -8,8 +8,8 @@ window.addEventListener('load', () => {
 class Application extends Component {
   constructor(props) {
     super(props)
-    this.state = {local: true, script: true};
-    ['setLocal'].forEach(prop => this[prop] = this[prop].bind(this))
+    this.state = {local: true, type: 'javascript'};
+    ['setLocal', 'setType'].forEach(prop => this[prop] = this[prop].bind(this))
   }
   render() {
     return (
@@ -20,8 +20,8 @@ class Application extends Component {
             <th><button onClick={this.setLocal.bind(null, false)} className={'local ' + (this.state.local === false ? 'active' : 'inactive')}>All Domains</button></th>
           </tr>
           <tr>
-            <th></th>
-            <th></th>
+            <th><button onClick={this.setType.bind(null, 'javascript')} className={'kind ' + (this.state.type === 'javascript' ? 'active' : 'inactive')}>JS</button></th>
+            <th><button onClick={this.setType.bind(null, 'css')} className={'kind ' + (this.state.type === 'css' ? 'active' : 'inactive')}>CSS</button></th>
           </tr>
         </table>
       </main>
@@ -29,5 +29,8 @@ class Application extends Component {
   }
   setLocal(local) {
     this.setState({...this.state, local: local === true})
+  }
+  setType(type) {
+    this.setState({...this.state, type})
   }
 }
